42598f258cb4cba88f60308606c2485f63065e7f
Necrospartan <76457537+Necrospartan@users.noreply.github.com>
2025-06-04 16:52:01 -0600

    Firt commit

---
+++ src/main/java/com/schoolmanager/academic_platform_api/AcademicPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.schoolmanager.academic_platform_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicPlatformApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schoolmanager/academic_platform_api/AcademicPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolmanager.academic_platform_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
